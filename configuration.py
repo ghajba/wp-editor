02a747813ff570a784c2f05ff2033ba615d1cf69
Gabor <gabor.hajba@gmail.com>
2015-01-22 06:36:29 +0100

    initial configuration added

---
+++ configuration.py
@@ -0,0 +1,36 @@
+"""
+This file loads and maintains the configuration of the application
+"""
+
+
+class Configuration:
+    def __init__(self, marker, begin, end=None):
+        self.marker = marker
+        self.begin = begin
+        self.end = end
+
+    def __str__(self):
+        return self.marker + " --> " + self.begin + " " +  self.end
+
+sourcecode_added = False
+configuration = []
+
+
+def load_configuration(config_file="mdxml.conf"):
+    config = open(config_file)
+    config_lines = config.readlines()
+    for line in config_lines:
+        if len(line.strip()) == 0:
+            continue
+        params = line.split()
+        marker = params[0]
+        begin = params[1]
+        if len(params) >= 3:
+            end =params[2]
+        else:
+            end =None
+        configuration.append(Configuration(marker, begin, end))
+
+def print_configuration():
+    for configuration_element in configuration:
+        print configuration_element
