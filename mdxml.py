687c6b1c996cc4952787efa4da24796d6f80f9b2
Gabor <gabor.hajba@gmail.com>
2015-01-22 18:36:29 +0100

    some changes on the conversion

--- mdxml.py
+++ mdxml.py
@@ -1,11 +1,11 @@
 # ## This is the main module of the markdown to XML converter python module
-### (c) Gabor Laszlo Hajba 2015
+# ## (c) Gabor Laszlo Hajba 2015
 """
 The main idea behind this app is to take a markdown file, parse it line-by-line and create an XML output
 along the defined rules of the conversion.
 """
 
-from configuration import Configuration, ConfigurationElement
+from configuration import Configuration
 import re
 
 
@@ -19,8 +19,10 @@ def convert_line(line):
     :param line: the line to convert
     :return: the line formatted as XML
     """
-    xmlline = line
-    config = None
+    xmlline = line.strip()
+    if len(xmlline) == 0:
+        return xmlline
+
     for marker in sorted(configuration.configuration, key=len, reverse=True):
         config_element = configuration.get_configuration(marker)
         xmlline = xmlline.replace(marker, config_element.begin)
@@ -36,14 +38,22 @@ def convert_line(line):
     return xmlline
 
 
-def read_mdfile():
-    pass
+def read_mdfile(filename):
+    input_file = open(filename)
+    input_lines = input_file.readlines()
+    input_file.close()
+
+    output_file = open(filename.rsplit('.', 1)[0]+".xml", 'w')
+    for line in input_lines:
+        if len(line.strip()) == 0:
+            continue
+        output_file.write(convert_line(line))
+        output_file.write("\n")
+        output_file.flush()
+    output_file.close()
 
 
 configuration = Configuration()
 configuration.load_configuration()
 
-configuration.print_configuration()
-
-print convert_line("#### This is ***a*** *test* header")
-print convert_line("This is ***a*** *test* paragraph")
+read_mdfile("testfile.md")
